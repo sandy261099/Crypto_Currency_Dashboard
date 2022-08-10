344231fb5350d4847940330695ff1e675991cc9f
Sai Sandeep Nangineni <saisandeepnangineni@Sais-MacBook-Pro.local>
2022-08-09 17:09:54 -0700

    new ui

--- src/App.js
+++ src/App.js
@@ -1,120 +1,78 @@
-import React, { useState, useEffect, useRef } from "react";
-import Dashboard from "./components/Dashboard";
-import { formatData } from "./utils";
-import "./styles.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+import Coin from './Coin';
 
-export default function App() {
-  const [currencies, setcurrencies] = useState([]);
-  const [pair, setpair] = useState("");
-  const [price, setprice] = useState("0.00");
-  const [pastData, setpastData] = useState({});
-  const ws = useRef(null);
-
-  let first = useRef(false);
-  const url = "https://api.pro.coinbase.com";
-
+function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
-    ws.current = new WebSocket("wss://ws-feed.pro.coinbase.com");
-
-    let pairs = [];
-
-    const apiCall = async () => {
-      await fetch(url + "/products")
-        .then((res) => res.json())
-        .then((data) => (pairs = data));
-
-      let filtered = pairs.filter((pair) => {
-        if (pair.quote_currency === "USD") {
-          return pair;
-        }
-      });
-
-      filtered = filtered.sort((a, b) => {
-        if (a.base_currency < b.base_currency) {
-          return -1;
-        }
-        if (a.base_currency > b.base_currency) {
-          return 1;
-        }
-        return 0;
-      });
-
-      console.log(filtered);
-      setcurrencies(filtered);
-
-      first.current = true;
-    };
-
-    apiCall();
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+    )
+      .then(res => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch(error => console.log(error));
   }, []);
 
-  useEffect(() => {
-    if (!first.current) {
-
-      return;
-    }
-
-
-    let msg = {
-      type: "subscribe",
-      product_ids: [pair],
-      channels: ["ticker"]
-    };
-    let jsonMsg = JSON.stringify(msg);
-    ws.current.send(jsonMsg);
-
-    let historicalDataURL = `${url}/products/${pair}/candles?granularity=86400`;
-    const fetchHistoricalData = async () => {
-      let dataArr = [];
-      await fetch(historicalDataURL)
-        .then((res) => res.json())
-        .then((data) => (dataArr = data));
-
-      let formattedData = formatData(dataArr);
-      setpastData(formattedData);
-    };
-
-    fetchHistoricalData();
-
-    ws.current.onmessage = (e) => {
-      let data = JSON.parse(e.data);
-      if (data.type !== "ticker") {
-        return;
-      }
-
-      if (data.product_id === pair) {
-        setprice(data.price);
-      }
-    };
-  }, [pair]);
-
-  const handleSelect = (e) => {
-    let unsubMsg = {
-      type: "unsubscribe",
-      product_ids: [pair],
-      channels: ["ticker"]
-    };
-    let unsub = JSON.stringify(unsubMsg);
-
-    ws.current.send(unsub);
-
-    setpair(e.target.value);
+  const handleChange = e => {
+    setSearch(e.target.value);
   };
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="container">
-      {
-        <select name="currency" value={pair} onChange={handleSelect}>
-          {currencies.map((cur, idx) => {
-            return (
-              <option key={idx} value={cur.id}>
-                {cur.display_name}
-              </option>
-            );
-          })}
-        </select>
-      }
-      <Dashboard price={price} data={pastData} />
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input
+            className='coin-input'
+            type='text'
+            onChange={handleChange}
+            placeholder='Search'
+          />
+        </form>
+      </div>
+      <div className="coin-container">
+        <div className="coin-row">
+          <div className="coin">
+            <h1>Name </h1>
+
+          </div>
+          <div className="coin-data">
+            <p className='coin-price'>Price </p>
+            <p className='coin-market-cap'>Volume</p>
+            <p className='coin-percent'>% Change </p>
+            <p className='coin-volume'>Market Cap</p>
+            <p className='coin-price'>High(24h)</p>
+            <p className='coin-price'>Low(24h)</p>
+
+          </div>
+        </div>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+            high={coin.high_24h}
+            low={coin.low_24h}
+          />
+        );
+      })}
     </div>
   );
 }
+
+export default App;
